0076120bff3db378d438d67bed4363ed07147026
sukhbeer <sukhbeer940@gmail.com>
2019-05-26 18:02:08 +0530

    jetpack

---
+++ AndroidJetpack/NavigationComponent/app/src/main/java/com/example/android/navigation/AccountFragment.kt
@@ -0,0 +1,32 @@
+package com.example.android.navigation
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.fragment_account.*
+
+
+class AccountFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_account, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        okBtn.setOnClickListener {
+            val name = Bundle()
+            name.putString("nameArg",editText.text.toString())
+            it.findNavController().navigate(R.id.nameFragment,name)
+        }
+    }
+}
